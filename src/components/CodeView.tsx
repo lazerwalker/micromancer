c543a7c6a62284c06dc1a1bdf043e8847bcc724f
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-28 21:47:53 +0200

    Highlight current line

--- src/components/CodeView.tsx
+++ src/components/CodeView.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Line } from "../Line";
+import classNames from "classnames";
 
 interface Props {
   code: Line[];
@@ -8,8 +9,11 @@ interface Props {
 
 export default function(props: Props) {
   const codeLines = props.code.map((l, i) => {
+    const klass = classNames("code-line", {
+      "current-line": i === props.currentLine
+    });
     return (
-      <div className="code-line" key={`line-${i}`}>
+      <div className={klass} key={`line-${i}`}>
         <span className="line-num relative">{i - props.currentLine}</span>
         <span className="line-num absolute">{i}</span>
         <span className="code-instruction">{l.join(" ")}</span>
