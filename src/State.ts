49aaefefb6267be90cb1863853a11fc50a7bedaf
Mike Lazer-Walker <michael@lazerwalker.com>
2019-06-29 00:06:02 +0200

    Always show a min of 10 lines

--- src/State.ts
+++ src/State.ts
@@ -6,8 +6,12 @@ export interface State {
 }
 
 export const initialState = (props?: Partial<State>): State => {
+  const code = [];
+  for (let i = 0; i < 10; i++) {
+    code.push([]);
+  }
   return {
-    code: [[]],
+    code,
     cursor: { line: 0, token: 0, inProgress: false },
     ...props
   };
