910580f01c883333bd5425396d36a43a4ffdbf99
Em Lazer-Walker <me@lazerwalker.com>
2019-07-22 15:24:34 +0200

    Updates

---
+++ precache-manifest.9a914a93d0385ced17ccfadecfe7c734.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8095137341934658b4b84f8b9d6d02f3",
+    "url": "/micromancer/index.html"
+  },
+  {
+    "revision": "ac1a37812c140b363c2b",
+    "url": "/micromancer/static/css/main.459b8c49.chunk.css"
+  },
+  {
+    "revision": "4db414c6692f9f5b043c",
+    "url": "/micromancer/static/js/2.b6a2c784.chunk.js"
+  },
+  {
+    "revision": "ac1a37812c140b363c2b",
+    "url": "/micromancer/static/js/main.17c07d54.chunk.js"
+  },
+  {
+    "revision": "79ed29278c120aa231cf",
+    "url": "/micromancer/static/js/runtime~main.07f933db.js"
+  },
+  {
+    "revision": "5af6402945d061207679afe65537638e",
+    "url": "/micromancer/static/media/ChevyRay - Chocolate Mono.5af64029.ttf"
+  },
+  {
+    "revision": "3b1066e28fd662ef25c6d0b76caa2038",
+    "url": "/micromancer/static/media/ChevyRay - Thicket Mono.3b1066e2.ttf"
+  },
+  {
+    "revision": "d228fa37180443f2fc19671530972763",
+    "url": "/micromancer/static/media/Gamegirl.d228fa37.ttf"
+  },
+  {
+    "revision": "069728a2c959cafbf691402fb2a265a4",
+    "url": "/micromancer/static/media/MicrogrammaDMedExt Regular.069728a2.ttf"
+  },
+  {
+    "revision": "292ff015e3a3b3e95b50d01d1bb9e59f",
+    "url": "/micromancer/static/media/barcadenobar.292ff015.ttf"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/micromancer/precache-manifest.9a914a93d0385ced17ccfadecfe7c734.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/micromancer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
